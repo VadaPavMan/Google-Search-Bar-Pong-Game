6af8f010ef65a04434a18313a876371d0a292f92
Harsh Rajbhar <119682671+VadaPavMan@users.noreply.github.com>
2024-11-01 19:26:54 +0530

    Update mainmenu.cpp
    New Mainmenu Added

--- mainmenu.cpp
+++ mainmenu.cpp
@@ -3,20 +3,20 @@
 #include <conio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <cstdlib>
 #include <time.h>
 #include <iostream>
 
 using namespace std;
 
-const int screenWidth = 1600, screenHeight = 900;
-float lastBackgroundSwitch = 0.0f;
-bool showFirstBackground = true;
-
-int main(void)
+int mainSecond(void)
 {
+    const int screenWidth = 1600, screenHeight = 900;
+    float lastBackgroundSwitch = 0.0f;
+    bool showFirstBackground = true;
     // Menu Code
-    char choose[300] = "Choose The Mode\n\n\n1.\t  EASY\t    [Ez Pz.]\n\n\n2.\t MEDIUM\t [For Casuals.]\n\n\n3.\t HARD\t    [Impossible To Defeat.]";
-    char select[100] = "Press Numbers 1 , 2 , 3\n\n\nAccordingly For Choosing The Mode.";
+    char choose[300] = "Choose The Mode\n\n\n1.\t  EASY\t    [Ez Pz.]\n\n\n2.\t MEDIUM\t [For Casuals.]\n\n\n3.\t HARD\t    [Impossible To Defeat.]\n\n\n4.\t 2 Players\t    [For PvP]";
+    char select[100] = "Press Numbers 1 , 2 , 3 And 4\n\n\nAccordingly For Choosing The Mode.";
 
     InitWindow(screenWidth, screenHeight, "Google Pong Game");
     SetTargetFPS(60);
@@ -31,7 +31,6 @@ int main(void)
 
     while (!WindowShouldClose())
     {
-
         // Background Refresh In 3 Sec
         if (GetTime() - lastBackgroundSwitch >= 3.0f)
         {
@@ -54,10 +53,40 @@ int main(void)
             DrawText(select, 490, 680, 40, WHITE);
         }
 
+        if (IsKeyDown(KEY_KP_1) || IsKeyDown(KEY_ONE))
+        {
+            UnloadTexture(menubgb);
+            UnloadTexture(menubgw);
+            CloseWindow();
+            return 1;
+        }
+        if (IsKeyDown(KEY_KP_2) || IsKeyDown(KEY_TWO))
+        {
+            UnloadTexture(menubgb);
+            UnloadTexture(menubgw);
+            CloseWindow();
+            return 2;
+        }
+        if (IsKeyDown(KEY_KP_3) || IsKeyDown(KEY_THREE))
+        {
+            UnloadTexture(menubgb);
+            UnloadTexture(menubgw);
+            CloseWindow();
+            return 3;
+        }
+        if (IsKeyDown(KEY_KP_4) || IsKeyDown(KEY_FOUR))
+        {
+            UnloadTexture(menubgb);
+            UnloadTexture(menubgw);
+            CloseWindow();
+            return 4;
+        }
+
         EndDrawing();
     }
 
     UnloadTexture(menubgb);
     UnloadTexture(menubgw);
-    return 0;
+    CloseWindow();
+    return false;
 }
