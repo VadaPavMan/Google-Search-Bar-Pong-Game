76722b31c7e990d206245f9fdc7c2c13d5ce5a89
Harsh Rajbhar <119682671+VadaPavMan@users.noreply.github.com>
2024-11-01 19:26:00 +0530

    Update compilecode.ps1

--- compilecode.ps1
+++ compilecode.ps1
@@ -7,3 +7,9 @@ gcc mainmenu.c -o game.exe -Wall -std=c99 -Wno-missing-braces -I include/ -L lib
 gcc game.cpp -o game.exe -Wall -Wno-missing-braces -I include/ -L lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
 
 gcc mainmenu.cpp -o game.exe -Wall -Wno-missing-braces -I include/ -L lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
+
+//Final Project:
+
+gcc mainmenu.c  game.c -o game.exe -Wall -Wno-missing-braces -I include/ -L lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
+
+gcc mainmenu.cpp  game.cpp -o game.exe -Wall -Wno-missing-braces -I include/ -L lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
